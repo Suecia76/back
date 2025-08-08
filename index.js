9f754ed1517e05ff590c9d9ee08fd974bcc95053
Suecia76 <carrascosanti76@gmail.com>
2025-08-08 16:01:19 -0300

    fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use("/uploads", express.static(path.resolve("uploads")));
+app.use("/uploads", cors(), express.static(path.resolve("uploads")));
 
 // Iniciar servidor
 const PORT = 3000;
